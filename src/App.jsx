dcfab95406f48c2f5ef3f8cba48d22f0af62a0e3
keezeem <azeemolajide7@gmail.com>
2024-12-10 14:10:49 +0100

    package.json code added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Registration from './components/registration'
+import Wallet from './components/Wallet'
+
+function App() {
+
+  return (
+    <div>
+        <Wallet />
+        {/* <Registration /> */}
+    </div>
+  )
+}
+
+export default App
